46e204d9c2712c3a6c27bdcbb43631863a21e6a6
oyc9904 <okoyann1@gmail.com>
2026-01-15 15:45:50 +0100

    Update DemoGithubApplication.java

--- demo-github/src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ demo-github/src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -2,12 +2,21 @@ package com.example.demo_github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoGithubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoGithubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubApplication.class, args);
+    }
 
+    @RestController
+    class HelloController {
+        @GetMapping("/")
+        public String hello() {
+            return "Hello, World!";
+        }
+    }
 }
